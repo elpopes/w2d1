1c5675184c238090d5e01c02201bbd8c914fa3de
lorenzo <lorenzotijerina@gmail.com>
2022-09-19 17:29:06 -0400

    errors with bootcamp initialization

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,70 @@
 class Bootcamp
-  
+    def initialize(name, slogan, student_capacity)
+        @name
+        @slogan
+        @student_capacity
+    end
+
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def slogan
+        @slogan
+    end
+
+    def slogan=(slogan)
+
+    end
+
+    def student_capacity
+        @student_capacity
+    end
+
+    def student_capacity=(student_capacity)
+
+    end
+
+    def teachers
+        @teachers
+    end
+
+    def teachers=(teachers)
+
+    end
+
+    def students
+        @students
+    end
+
+    def students=(students)
+
+    end
+
+
 end
+
+#     def hire()
+#     end
+
+#     def enroll()
+#     end
+
+#     def enrolled?()
+#     end
+
+#     def teacher_to_student_ratio()
+#     end
+
+
+
+
+
+# grades = Hash.new { |hash, k| hash[k]}
+
+
+# end
